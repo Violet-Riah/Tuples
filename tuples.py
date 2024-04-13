5c7a28c9710a5b725478b9da3ce9163bdc750eb7
Violet-Riah <violettanjombwa@gmail.com>
2024-04-13 10:48:26 +0300

    add file

---
+++ tuples.py
@@ -0,0 +1,13 @@
+tuple = (1,2,3,4,5,6)
+
+print(tuple[1])
+print (tuple[4])
+
+tuples = (1,2,3,4,5,6)
+print(tuple)
+
+tuple = ("green","yellow","blue","pink","black")
+
+print ("value in tuple[0] = ", tuple[0])
+print ("value in puple[1] = ", tuple[3])
+print ("value in tuple[2] =", tuple[4])
